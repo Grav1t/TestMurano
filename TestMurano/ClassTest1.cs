3b4d05193e36b79abc10e51af462ff28e37e5666
Konstantin <rooveredj@gmail.com>
2021-05-26 03:08:41 +0300

    Basic build and test case

--- TestMurano/ClassTest1.cs
+++ TestMurano/ClassTest1.cs
@@ -1,7 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,26 +10,111 @@ using System.Threading.Tasks;
 
 namespace TestMurano
 {
-    class ClassTest1
+    [TestFixture]
+    public class TestLogIn
     {
-        IWebDriver driver;
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private bool acceptNextAlert = true;
 
         [SetUp]
-        public void startBrowser()
+        public void SetupTest()
         {
             driver = new ChromeDriver("D:\\Program Files (x86)\\Google\\Chrome");
-        }
-
-        [Test]
-        public void test()
-        {
-            driver.Url = "http://www.google.co.in";
+            
+            verificationErrors = new StringBuilder();
         }
 
         [TearDown]
-        public void closeBrowser()
+        public void TeardownTest()
         {
-            driver.Close();
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheUntitledTestCaseTest()
+        {
+            driver.Navigate().GoToUrl("https://dotnetfiddle.net/");
+            driver.FindElement(By.Id("login-button")).Click();
+            driver.FindElement(By.Id("Email")).Click();
+            driver.FindElement(By.Id("Email")).Clear();
+            driver.FindElement(By.Id("Email")).SendKeys("k.test01@mail.ru");
+            driver.FindElement(By.Id("Password")).Click();
+            driver.FindElement(By.Id("Password")).Clear();
+            driver.FindElement(By.Id("Password")).SendKeys("TestPass001");
+            driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+           // driver.FindElement(By.XPath("//form[@id='CodeForm']/div[2]/div[4]/div/div/div[2]/div[6]")).Click();
+            try
+            {
+                Assert.AreEqual("C# Online Compiler | .NET Fiddle", driver.Title);
+            }
+            catch (AssertionException e)
+            {
+                verificationErrors.Append(e.Message);
+            }
+            /*try
+            {
+                Assert.AreEqual("Konsta", driver.FindElement(By.Id("account-display-name")).Text);
+            }
+            catch (AssertionException e)
+            {
+                verificationErrors.Append(e.Message);
+            }*/
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
         }
     }
 }
